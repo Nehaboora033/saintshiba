313289f817e79a58b143648fe1e0280100bf4973
Neha <nehaboora49@gmail.com>
2025-06-10 14:24:31 +0530

    making responsive

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+// accordion
+ 
+function toggleAccordion(selector) {
+    const clickedItem = document.querySelector(selector);
+    const allItems = document.querySelectorAll('.accordion-item');
+
+    allItems.forEach(item => {
+        if (item !== clickedItem) {
+            item.classList.remove('active');
+        }
+    });
+
+    clickedItem.classList.toggle('active');
+}
+
+// nav bar
+    window.onscroll = function () {
+        var nav = document.querySelector("nav");
+        if (window.scrollY > 1) { // Adjust scroll trigger
+            nav.classList.add("scrolled");
+        } else {
+            nav.classList.remove("scrolled");
+        }
+    };
+
+
+function menuIcon() {
+    document.querySelector('.menu-list').classList.toggle('show');
+    document.querySelector('nav').classList.toggle('activeNavBar');
+
+};
